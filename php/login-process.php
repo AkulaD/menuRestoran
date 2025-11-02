044597a3694e761a7dc4034a5cb9be7686267f1f
shaka <sh4k4175@gmail.com>
2025-11-02 20:01:01 +0700

    dikit lagi

--- php/login-process.php
+++ php/login-process.php
@@ -1,20 +1,36 @@
-<?php 
-include 'config.php';
-
+<?php
 session_start();
 
 $users = [
     "customer" => ["password" => "customer", "level" => 1],
-    "staff" => ["[password" => "admin1234", "level" => 2],
+    "staff"    => ["password" => "admin1234", "level" => 2],
 ];
 
-$username = $_POST['username']??'';
-$password = $_POST['password']??'';
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
 
-if($username === '' || $password === ''){
+if ($username === '' || $password === '') {
     echo "<script>alert('Username dan Password wajib diisi!'); window.history.back();</script>";
     exit;
 }
 
-if(isset($User[$username]) && $users[$username]['password'] === $password)
+if (isset($users[$username]) && $users[$username]['password'] === $password) {
+    $_SESSION['username'] = $username;
+    $_SESSION['level'] = $users[$username]['level'];
+    $level = $_SESSION['level'];
+
+    if ($level == 1) {
+        header('Location: ../dashboard/dashboard.php');
+        exit;
+    } elseif ($level == 2) {
+        header('Location: ../menu/index.html');
+        exit;
+    } else {
+        echo "<script>alert('Level tidak dikenali!'); window.history.back();</script>";
+        exit;
+    }
+} else {
+    echo "<script>alert('Username atau Password salah!'); window.history.back();</script>";
+    exit;
+}
 ?>
